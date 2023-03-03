e3b669c54c49ee8e4eebe73cb9d9f977405e5786
smarterdegen <49041460+smarterdegen@users.noreply.github.com>
2023-03-03 10:59:07 -0800

    chore: remove unused components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
